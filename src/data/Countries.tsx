51422376abc4b13d4069e0e559e3e1101972b179
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-01 18:22:56 +0100

    Modularized code for data

---
+++ src/data/Countries.tsx
@@ -0,0 +1,16 @@
+export const countries: Countries = {
+  at: "Austria",
+  au: "Australia",
+  ca: "Canada",
+  ch: "Switzerland",
+  de: "Germany",
+  dk: "Denmark",
+  in: "India",
+  nl: "Netherlands",
+  no: "Norway",
+  se: "Sweden",
+  uk: "United Kingdom",
+  us: "United States",
+}
+
+export const countryCodes: string[] = Object.keys(countries);
--- src/data/JobBoards.tsx
+++ src/data/JobBoards.tsx
@@ -1,44 +1,13 @@
-type Countries = { [key: string]: string; }
-type Urls = { base: string; countries: Countries }
-type JobBoard = { name: string; urls: Urls; }
-type JobBoardsProps = { [key: string]: JobBoard; }
-type Country = { [key: string]: string; }
+import {countryCodes} from "./Countries.tsx";
+import {RegionalUrls} from "./scripts.tsx";
 
-
-const countries: Country = {
-  at: "Austria",
-  au: "Australia",
-  ca: "Canada",
-  ch: "Switzerland",
-  de: "Germany",
-  dk: "Denmark",
-  in: "India",
-  nl: "Netherlands",
-  no: "Norway",
-  se: "Sweden",
-  uk: "United Kingdom",
-  us: "United States",
-}
-
-const countryCodes: string[] = Object.keys(countries);
-
-function CountryUrls(domain: string, tld: string, countryCodes: string[]): Record<string, string> {
-  const urls: Record<string, string> = {};
-  countryCodes.forEach(code => {
-    let subdomain: string = code === 'us' ? "www" : code;
-    urls[code] = `https://${subdomain}.${domain}.${tld}/`;
-  });
-  return urls;
-}
-
-
-const jobBoards: JobBoardsProps = {
+export const jobBoards: JobBoards = {
   indeed: {
     name: "Indeed",
-    urls: {base: "https://www.indeed.com/", countries: CountryUrls("indeed", "com", countryCodes)},
+    urls: {base: "https://www.indeed.com/", countries: RegionalUrls("indeed", "com", countryCodes)},
   },
   linkedin: {
     name: "LinkedIn",
-    urls: {base: "https://www.linkedin.com/", countries: CountryUrls("linkedin", "com", countryCodes)},
+    urls: {base: "https://www.linkedin.com/", countries: RegionalUrls("linkedin", "com", countryCodes)},
   },
 }
