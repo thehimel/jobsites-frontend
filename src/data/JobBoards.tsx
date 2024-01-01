6db60d00ff92b11d13e31a18001f03948a30f19b
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-01 20:25:44 +0100

    Updated dataset for jobboards

--- src/data/JobBoards.tsx
+++ src/data/JobBoards.tsx
@@ -10,4 +10,26 @@ export const jobBoards: JobBoards = {
     name: "LinkedIn",
     urls: {base: "https://www.linkedin.com/", countries: RegionalUrls("linkedin", "com", countryCodes)},
   },
-}
+  careerbuilder: {
+    name: "CareerBuilder",
+    urls: {
+      base: "https://www.careerbuilder.com/",
+      countries: {
+        ca: "https://www.careerbuilder.ca/",
+        in: "https://www.careerbuilder.co.in/",
+        us: "https://www.careerbuilder.com/",
+      },
+    },
+  },
+  ziprecruiter: {
+    name: "ZipRecruiter",
+    urls: {
+      base: "https://www.ziprecruiter.com/",
+      countries: {
+        ca: "https://www.ziprecruiter.ca/",
+        uk: "https://www.ziprecruiter.co.uk/",
+        us: "https://www.ziprecruiter.com/",
+      },
+    },
+  },
+};
--- src/data/index.tsx
+++ src/data/index.tsx
@@ -2,4 +2,3 @@ type Countries = { [key: string]: string; }
 type Urls = { base: string; countries: Countries }
 type JobBoard = { name: string; urls: Urls; }
 type JobBoards = { [key: string]: JobBoard; }
-
