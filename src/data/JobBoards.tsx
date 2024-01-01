3b3542a7eca84499cbce7b2ba272625e3d79a167
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-01 18:07:52 +0100

    Updated dataset for JobBoards.tsx

--- src/data/JobBoards.tsx
+++ src/data/JobBoards.tsx
@@ -1,26 +1,50 @@
-type Countries = { [key: string]: string; }
-type Urls = { default: string; countries: Countries }
-type JobBoard = { name: string; urls: Urls; }
+type Region = { [key: string]: string; }
+type Url = { base: string; region: Region }
+type JobBoard = { name: string; url: Url; }
 type JobBoardsProps = { [key: string]: JobBoard; }
+type Country = { [key: string]: string; }
 
-export const jobBoards: JobBoardsProps = {
+
+const countries: Country = {
+  at: "Austria",
+  au: "Australia",
+  ca: "Canada",
+  ch: "Switzerland",
+  de: "Germany",
+  dk: "Denmark",
+  in: "India",
+  nl: "Netherlands",
+  no: "Norway",
+  se: "Sweden",
+  uk: "United Kingdom",
+  us: "United States",
+}
+
+const countryCodes: string[] = Object.keys(countries);
+
+function CountryUrls(domain: string, tld: string, countryCodes: string[]): Record<string, string> {
+  const urls: Record<string, string> = {};
+  countryCodes.forEach(code => {
+    let subdomain: string = code === 'us' ? "www" : code;
+    urls[code] = `https://${subdomain}.${domain}.${tld}/`;
+  });
+  return urls;
+}
+
+
+const jobBoards: JobBoardsProps = {
   indeed: {
     name: "Indeed",
-    urls: {
-      default: "https://www.indeed.com/",
-      countries: {
-        at: "https://at.indeed.com/",
-        au: "https://au.indeed.com/",
-        ca: "https://ca.indeed.com/",
-        ch: "https://ch.indeed.com/",
-        de: "https://de.indeed.com/",
-        dk: "https://dk.indeed.com/",
-        nl: "https://nl.indeed.com/",
-        no: "https://no.indeed.com/",
-        se: "https://se.indeed.com/",
-        uk: "https://uk.indeed.com/",
-        us: "https://www.indeed.com/",
-      },
+    url: {
+      base: "https://www.indeed.com/",
+      region: CountryUrls("indeed", "com", countryCodes)
+    },
+  },
+  linkedin: {
+    name: "LinkedIn",
+    url: {
+      base: "https://www.linkedin.com/",
+      region: CountryUrls("linkedin", "com", countryCodes)
     },
   },
 }
