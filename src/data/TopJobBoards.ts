7b819aea94e760163fdf37de2b1e8720f6fa6a2c
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-01 23:19:54 +0100

    Created data on top job boards

---
+++ src/data/TopJobBoards.ts
@@ -0,0 +1,22 @@
+import {jobBoards} from "./JobBoards.ts";
+
+export const topJobBoards = {
+  us: [
+    jobBoards.indeed,
+    jobBoards.linkedin,
+    jobBoards.careerbuilder,
+    jobBoards.ziprecruiter,
+    jobBoards.glassdoor,
+    jobBoards.monster,
+    jobBoards.getwork,
+    jobBoards.flexjobs,
+  ],
+  uk: [
+    jobBoards.indeed,
+    jobBoards.linkedin,
+    jobBoards.monster,
+    jobBoards.glassdoor,
+    jobBoards.ziprecruiter,
+    jobBoards.getwork,
+  ],
+}
