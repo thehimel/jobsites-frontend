9b9f14c3e8c9ed9f1b143b95eb62d8db202e7e06
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-01 19:21:53 +0100

    Refactored filterArrayByWord()

--- src/scripts/utilities.tsx
+++ src/scripts/utilities.tsx
@@ -4,10 +4,5 @@ export const capitalize = (str: string): string => {
 
 export const filterArrayByWord = (elements: string[], word: string): string[] => {
   const wordLower = word.toLowerCase();
-  if (!word.length) {
-    return elements;
-  }
-  return elements
-    .filter(element => element.toLowerCase().includes(wordLower))
-    .map(element => element);
+  return !word.length ? elements : elements.filter(element => element.toLowerCase().includes(wordLower));
 };
