14d6d6b519241b437dc1d7c127d00d5fdcb9b708
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-04 21:02:12 +0100

    Updated import of Bootstrap css and js

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from "react-redux";
 import store from "./store";
 
