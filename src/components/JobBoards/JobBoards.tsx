e5e5890de6be06b071de21f0e6d744555c4d7cfe
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-05 02:57:33 +0100

    Added go back button on title of top job boards

--- src/components/JobBoards/JobBoards.tsx
+++ src/components/JobBoards/JobBoards.tsx
@@ -16,7 +16,10 @@ export default function JobBoards() {
   return (
     <>
       <div className="text-center pt-4">
-        <h4 className="d-inline-block text-bg-dark rounded px-4 py-2">{title}</h4>
+        <h4 className="d-inline-block text-bg-dark rounded px-4 py-2">
+          {title}
+          <a href="/" className="ps-2"><span className="badge text-bg-secondary">Go Back</span></a>
+        </h4>
       </div>
       <div className={rowClasses}>
         {jobBoards}
