f12fccd0c8fc6a61f2667b8429128c87933324d3
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-05 02:41:28 +0100

    Added title in the JobBoards page

--- src/components/JobBoards/JobBoards.tsx
+++ src/components/JobBoards/JobBoards.tsx
@@ -3,6 +3,7 @@ import JobBoard from "./JobBoard.tsx";
 import {rowClasses} from "../Styles.ts";
 import {useSelector} from "react-redux";
 import {CountriesState} from "../../constants/core.tsx";
+import {getCountryNameFromCode} from "../../data/Countries.ts";
 
 export default function JobBoards() {
   const code: string = useSelector((state: CountriesState) => state.country.selectedCode);
@@ -10,9 +11,13 @@ export default function JobBoards() {
     (element: JobBoard) => <JobBoard key={element.name} name={element.name}
                                      url={element.urls[code]}></JobBoard>
   );
+  const title: string = `Top Job Boards in ${getCountryNameFromCode(code)}`;
 
   return (
     <>
+      <div className="text-center pt-4">
+        <h4 className="d-inline-block text-bg-dark rounded px-4 py-2">{title}</h4>
+      </div>
       <div className={rowClasses}>
         {jobBoards}
       </div>
