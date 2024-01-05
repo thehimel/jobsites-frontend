609b7b7e16111605019e97b01cc96c05ec63c3f5
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-01-05 01:01:41 +0100

    Added onClick method for country component

--- src/components/Countries/Country.tsx
+++ src/components/Countries/Country.tsx
@@ -3,9 +3,13 @@ interface CountryProps {
 }
 
 const Country = ({title}: CountryProps) => {
+  function handleClick(country: string): void {
+    console.log(country);
+  }
+
   return (
     <div className="col g-2">
-      <button className="text-bg-dark rounded-3 w-100 h-100 p-5" type="button">
+      <button className="text-bg-dark rounded-3 w-100 h-100 p-5" type="button" onClick={() => handleClick(title)}>
         <h3>{title}</h3>
       </button>
     </div>
